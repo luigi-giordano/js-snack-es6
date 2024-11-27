97c9ab4069a8071a44c747cd797e621b0c10c3cb
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-27 23:09:30 +0100

    exercise bici completed - ciclo for of -

--- Bici/script.js
+++ Bici/script.js
@@ -2,19 +2,30 @@
 let biciclette = [
   { 
     nome: "Molonia",
-    peso: 7.5 
+    peso: 7.5,
   },
   { 
     nome: "Emonda",
-    peso: 6.8 
+    peso: 6.8,
   },
   { 
     nome: "Bernava",
-    peso: 6.7 
+    peso: 5.3, 
   },
   { 
     nome: "Superbike",
-    peso: 7.2 
+    peso: 7.2, 
   }
 ];
 
+// Bici con il peso minore
+let biciLeggera = biciclette[0];
+
+for (let bici of biciclette) {
+  if (bici.peso < biciLeggera.peso) {
+    biciLeggera = bici;
+  }
+}
+
+// Stampare a schermo la bici con peso minore
+console.log("La bici più leggera è:", biciLeggera.nome, "col peso di", biciLeggera.peso, "kg");
