f435f8e16583768f1b54b081832d40f3bab33ab4
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-27 23:03:10 +0100

    create array oggetti

---
+++ Bici/script.js
@@ -0,0 +1,20 @@
+// Creazione dell'array di oggetti
+let biciclette = [
+  { 
+    nome: "Molonia",
+    peso: 7.5 
+  },
+  { 
+    nome: "Emonda",
+    peso: 6.8 
+  },
+  { 
+    nome: "Bernava",
+    peso: 6.7 
+  },
+  { 
+    nome: "Superbike",
+    peso: 7.2 
+  }
+];
+
