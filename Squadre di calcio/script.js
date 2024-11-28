a9797efbaf650e6a80b555de796921a305c7fb23
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-28 10:24:41 +0100

    fix exercise in classroom

--- Squadre di calcio/script.js
+++ Squadre di calcio/script.js
@@ -1,10 +1,44 @@
 // Creazione dell'array di oggetti delle squadre di calcio
 let squadre = [
-  { nome: "Juventus", punti: 0, falliSubiti: 0 },
-  { nome: "Milan", punti: 0, falliSubiti: 0 },
-  { nome: "Inter", punti: 0, falliSubiti: 0 },
-  { nome: "Napoli", punti: 0, falliSubiti: 0 }
+  { 
+    nome: "Juventus",
+    punti: 0,
+    falliSubiti: 0 
+  },
+  { 
+    nome: "Milan",
+    punti: 0,
+    falliSubiti: 0 
+  },
+  { 
+    nome: "Inter",
+    punti: 0,
+    falliSubiti: 0 
+  },
+  { 
+    nome: "Napoli",
+    punti: 0,
+    falliSubiti: 0 
+  }
 ];
 
 // Funzione per generare numeri random
-let getNumbersRandom = (min, max) => Math.floor(Math.random() * (max - min + 1));
+let getNumbersRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+for(let squadra of squadre){
+  squadra.puntiFatti = getNumbersRandom(0,30);
+  squadra.falliSubiti = getNumbersRandom(0,30)
+}
+
+
+const nuoveSquadre = [];
+
+for(let squadra of squadre){
+  const nuovaSquadra = {
+    nome: squadra.nome,
+    falliSubiti: squadra.falliSubiti
+  }
+  nuoveSquadre.push(nuovaSquadra)
+
+}
+console.log(nuoveSquadre)
